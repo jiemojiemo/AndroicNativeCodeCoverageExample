d9d145f0314d10fdc1975da12ea17b66d771e845
jiemojiemo <460706836@qq.com>
2022-07-29 10:10:51 +0800

    clean code

--- app/src/main/java/com/example/nativecodecoveragetest/MainActivity.java
+++ app/src/main/java/com/example/nativecodecoveragetest/MainActivity.java
@@ -21,7 +21,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-    static String TAG = "MainActivityxxxx";
+    static String TAG = "MainActivity";
 
     static {
         System.loadLibrary("native-lib");
@@ -32,40 +32,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
-            // 最后的请求码是对应回调方法的请求码
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                            Manifest.permission.READ_EXTERNAL_STORAGE
-                    }, 1001);
-        } else {
-            Toast.makeText(this, "你已经有权限了，可以直接拨打电话", Toast.LENGTH_LONG).show();
-        }
-
         try {
-            String path = android.os.Environment.getExternalStorageDirectory().getAbsolutePath();
-            File savePath = new File(path, "Music");
-            Log.d(TAG, savePath.getAbsolutePath());
             Os.setenv("GCOV_PREFIX", getCacheDir().getAbsolutePath(), true);
             Os.setenv("GCOV_PREFIX_STRIP", "100", true);
         } catch (ErrnoException e) {
             e.printStackTrace();
         }
-
-        try {
-            FileOutputStream xx = new FileOutputStream("/data/local/tmp/gcov/abc");
-            xx.write(10);
-            xx.close();
-            Log.d(TAG, "write file");
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            Log.d(TAG, "write xxx");
-        } catch (IOException e) {
-            e.printStackTrace();
-            Log.d(TAG, "write yyy");
-        }
-
 
         int r = callNative();
         Log.d(TAG, "result is " + r);
